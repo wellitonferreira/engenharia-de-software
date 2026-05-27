faa98462a1832feeee4b5e7e6dbb933cbfef8fbc
Welliton Ferreira <wellitonmousinhoferreira@gmail.com>
2026-05-26 23:38:57 -0300

    Tipos de variáveis em C

---
+++ Variaveis/variaveis.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int idade = 25;
+    int quantidade = 1;
+    float altura = 1.75;
+    double peso = 90.3;
+    char letra = 'A';
+    char nome[20] = "Sergio";
+
+    //Posso fazer a substituição dos valores declarando novos valores
+    idade = 30;
+
+    printf("%d", idade);
+}
